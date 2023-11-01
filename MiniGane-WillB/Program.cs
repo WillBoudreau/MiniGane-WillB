afcdda47142b65694b87a0bfb18b378a4db78f08
WillBoudreau <williamboudreau804@gmail.com>
2023-11-01 15:28:30 -0300

    Still working on Program

--- MiniGane-WillB/Program.cs
+++ MiniGane-WillB/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Media;
 using System.Text;
@@ -11,48 +12,83 @@ namespace MiniGane_WillB
     {
 
         static string[] player;
+        
+        static int x=30;
+        static int y=30;
+        static int xmax = 30;
+        static int ymax = 15;
+        static int xmin = -30;
+        static int ymin = -15;
         static void Main(string[] args)
         {
-            player[0] = "-";
-            Console.WriteLine("MiniGame");
-            Console.WriteLine();
 
-            //Count 1 to 10
-            //for(int i =  1 ; i <= 10; i++)
-            {
-               // Console.WriteLine(i);
-            }
-            int x=10;
-            int y=10;
+            Console.SetWindowSize(xmax,ymax);
+            Console.WriteLine("MiniGame");
+            Console.ReadKey();
+            player = new string[2];
+            player[0] = "*";
+           
             bool gameOver = false;
-             while(gameOver != true)
+            while (gameOver != true)
             {
                 ConsoleKeyInfo input;
-               input = Console.ReadKey();
-                if(input.Key == ConsoleKey.W)//key on keyboard
+                input = Console.ReadKey();
+                
+                if (y > ymax)
+                {
+                    y = ymax;
+                } 
+                if(y < ymin)
+                {
+                    y = ymin;
+                }
+                if( x > xmax)
+                {
+                    x = xmax;
+                }
+                if( x < xmin)
+                {
+                    x = xmin;
+                }
+                PlayerDraw();
+                if (input.Key == ConsoleKey.W)//key on keyboard
                 {
                     y--;
+                            
                 }
-                if(input.Key == ConsoleKey.A) 
+                if (input.Key == ConsoleKey.A)
                 {
-                    Console.Clear();
-                    x--;
-                    
+                            
+                   x--;
+                            
                 }
-                if(input.Key == ConsoleKey.D)
+                if (input.Key == ConsoleKey.D)
                 {
-                    x++;
+                            
+                   x++;
+                            
                 }
-                if(input.Key == ConsoleKey.S)
+                if (input.Key == ConsoleKey.S)
                 {
+                            
                     y++;
+                            
                 }
-                Console.SetCursorPosition(x, y);
-                Console.Write(player[0]);
+                if (input.Key == ConsoleKey.Escape)
+                {
+                    gameOver = true;
+                }
+                
             }
             Console.WriteLine();
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
+        }
+        static void PlayerDraw()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(x, y);
+            Console.Write(player[0]);
         }
     }
 }
